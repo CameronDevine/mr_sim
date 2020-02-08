de03ceaaf03d54c69ae1b160fe639d56c8405688
Cameron Devine <camerondtrain@gmail.com>
2020-02-08 11:53:32 -0800

    added tests for the utility functions

---
+++ tests/test_util.py
@@ -0,0 +1,24 @@
+from context import mr_sim
+import unittest
+
+
+class TestUtil(unittest.TestCase):
+    def test_create(self):
+        class A:
+            a = 1
+
+        class B:
+            b = 2
+
+        class C:
+            c = 3
+
+        Test = mr_sim.create_simulation(A, B)
+        test = Test()
+        self.assertEqual(test.a, 1)
+        self.assertEqual(test.b, 2)
+        Test = mr_sim.create_simulation(A, B, C)
+        test = Test()
+        self.assertEqual(test.a, 1)
+        self.assertEqual(test.b, 2)
+        self.assertEqual(test.c, 3)
